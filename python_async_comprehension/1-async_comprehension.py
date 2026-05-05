0394152fc6b611056d4e8da580a2fa8453b61e16
Huntless937 <fidailveliyev09@gmail.com>
2026-05-05 18:14:15 +0000

    lastone

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 """
-Async comprehension tapşırığı
+Module for async comprehension task.
 """
-import asyncio
 from typing import List
 
-# Əvvəlki tapşırıqdan async_generator-u import edirik
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """
-    Async generator üzərində dövr edir və 10 təsadüfi rəqəmi 
-    list comprehension vasitəsilə toplayır.
+    Coroutine that collects 10 random numbers using an async
+    comprehension over async_generator and returns them.
     """
     return [i async for i in async_generator()]
