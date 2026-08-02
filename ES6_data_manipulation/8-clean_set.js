4df872d11e2d6fcfe4e6f9d219e681639b3012fb
Huntles937 <fidailveliyev09@gmail.com>
2026-08-02 21:56:40 +0200

    done

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+function cleanSet(set, startString) {
+    const result = [];
+  
+    set.forEach((value) => {
+      if (startString && value.startsWith(startString)) {
+        result.push(value.slice(startString.length));
+      }
+    });
+  
+    return result.join('-');
+  }
+  
+  export default cleanSet;
